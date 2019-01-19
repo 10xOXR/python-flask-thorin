b103c162c77b37fe8320d4be539c1650b3c62f16
Chris Quinn <10xoxr@gmail.com>
2019-01-19 19:23:39 +0000

    Created contact form and enabled function from flash method.

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "bear_fur_is_better"
 
 tabTitle = " | "
 
@@ -30,8 +31,10 @@ def about_member(member_name):
 
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method =="POST":
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
     pageTitle = "Contact"
     return render_template("contact.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
