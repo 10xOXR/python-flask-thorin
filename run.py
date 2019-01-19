425c870253d118c48ce6e2adbb11282173f98a84
Chris Quinn <10xoxr@gmail.com>
2019-01-19 16:53:55 +0000

    Added JSON data and more 'render_remplate' arguments.

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+tabTitle = " | "
 
 @app.route("/")
 def index():
@@ -9,15 +12,21 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    pageTitle = "About"
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title=pageTitle, tab_title=pageTitle + tabTitle, company = data)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    pageTitle = "Contact"
+    return render_template("contact.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    pageTitle = "Careers"
+    return render_template("careers.html", page_title=pageTitle, tab_title=pageTitle + tabTitle)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
